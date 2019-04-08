f8d308ca778fb936287a7a1cdb504a0ef1901b43
HelenMontes <48477888+HelenMontes@users.noreply.github.com>
2019-04-08 17:46:27 -0500

    Add files via upload

--- FRONTEND/js/Validaciones.js
+++ FRONTEND/js/Validaciones.js
@@ -3,9 +3,14 @@ function aplyValidations(){
   validarNombre();
   validarApellido();
   validarDocumento();
-  validarRadioButton();
+  validarFechaN();
+  validarDireccion();
+  validarTelefono();  
+  validarRadioButton(); 
+  validarSelectCiudad();  
   validarCorreo();
-  validarSelectCiudad();
+ 
+  
 }
 
 
@@ -17,6 +22,8 @@ function validarNombre(){
       return false;
     }
 }
+
+
 
 function validarApellido(){
   var apellido = document.getElementById('apellido').value;
@@ -32,6 +39,35 @@ function validarDocumento(){
 
   if (validarDocumento==0){
       alert("Campo Documento de Identificación vacio");
+      return false;
+    }
+}
+
+function validarFechaN(){
+  var validarFechaN = document.getElementById('fechanacimiento').value;
+
+  if (validarFechaN ==""){
+      alert("Campo Fecha Nacimiento vacio");
+      return false;
+    }
+}
+
+
+function validarDireccion(){
+  var validarDireccion = document.getElementById('direccionpersona').value;
+
+  if (validarDireccion ==""){
+      alert("Campo Dirección vacio");
+      return false;
+    }
+}
+
+
+function validarTelefono(){
+  var validarTelefono = document.getElementById('telefono').value;
+
+  if (validarTelefono ==""){
+      alert("Campo Telefono vacio");
       return false;
     }
 }
